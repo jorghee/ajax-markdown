1b68ae4952ac0dafdb9c115c10a4c9e28374d988
Jorghee <jorgeehuarsaya@gmail.com>
2024-05-18 11:55:33 -0500

    Configurando que el servidor pueda leer los archivos del directorio files_markdown

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+const path = require('path');
+const fs = require("fs");
+// const MarkdownIt = require("markdown-it");
+const bp = require("body-parser");
 const express = require("express");
+
 const app = express();
+// const md = new MarkdownIt();
+const MARKDOWN_DIR = path.resolve(__dirname, "files_markdown");
+
+app.use(bp.json());
+app.use(express.static("public"));
 
 /** 
   * Creamos el servidor, request maneja las solicitudes que hacemos y 
   * response es el objeto que maneja las respuestas que envia el servidor.
   */ 
 app.get('/', (request, response) => {
-  return response.send("Hello jijijaja");
+  response.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+// Listar archivos Markdown
+app.get("/files", (request, response) => {
+  fs.readdir(MARKDOWN_DIR, (error, files) => {
+    if (error) {
+      console.error("No se pudo leer los archivos:", error);
+      return;
+    }
+    response.json(files);
+  });
 });
 
 // 3000 es el puerto
